cef3360626571f18637136370824620c6dacac1e
tikkhun <s565338107@163.com>
2024-06-17 21:18:03 +0800

    chore: just vue-ui could build. but not d.ts...

--- packages/vue-ui/src/App.vue
+++ packages/vue-ui/src/App.vue
@@ -1,35 +1,47 @@
-import TheTime from '../dist/components/TheTime';
-<!--
- 
-/**
- * @author
- * @file App.vue
- * @fileBase App
- * @path packages\vue-ui\src\App.vue
- * @from 
- * @desc App
- * @todo
- * 
- *
- * @done
- * @example
- */
-
--->
-
-<script lang="ts">
-export default {
-  name: "App",
-};
-</script>
 <script setup lang="ts">
-import { TheTime } from "../lib";
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <div class="App">
-    <TheTime></TheTime>
-  </div>
+  <header>
+    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <main>
+    <TheWelcome />
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
--- packages/vue-ui/vite.config.ts
+++ packages/vue-ui/vite.config.ts
@@ -1,37 +1,34 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import { defineConfig } from 'vite'
 
-import packageJson from "./package.json";
-import { fileURLToPath, URL } from "node:url";
-
-const name = packageJson.name.split("/")[1];
-// https://vitejs.dev/config/
+import packageJson from './package.json'
+import { fileURLToPath, URL } from 'node:url'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+const name = packageJson.name.split('/')[1]
 export default defineConfig({
   plugins: [vue(), vueJsx()],
   build: {
     lib: {
-      entry: "./lib/index.ts",
+      entry: './lib/index.ts',
       name: toBigCamelCase(name),
-      fileName: name,
+      fileName: name
     },
     rollupOptions: {
       output: {
         globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-        },
+          vue: 'Vue'
+        }
       },
-      external: ["react", "react-dom", "react-router", "react-router-dom"],
-    },
+      external: ['vue']
+    }
   },
   resolve: {
     alias: {
-      "@": fileURLToPath(new URL("./lib", import.meta.url)),
-    },
-  },
-});
+      '@': fileURLToPath(new URL('./lib', import.meta.url))
+    }
+  }
+})
 export function toBigCamelCase(str: string) {
-  str = str.replace(/[-_](\w)/g, (_, c) => c.toUpperCase()); // 将连字符和下划线后的字符转换为大写
-  return str.replace(/^\w/, (c) => c.toUpperCase()); // 将首字母转换为大写
+  str = str.replace(/[-_](\w)/g, (_, c) => c.toUpperCase()) // 将连字符和下划线后的字符转换为大写
+  return str.replace(/^\w/, (c) => c.toUpperCase()) // 将首字母转换为大写
 }
