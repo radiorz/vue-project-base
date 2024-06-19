967c898755ee658a88f963a034225e35ac9c715f
tikkhun <s565338107@163.com>
2024-06-19 10:49:06 +0800

    chore: 抽象函数。

--- packages/vue-ui/lib/MaxHeight/useMaxHeight.ts
+++ packages/vue-ui/lib/MaxHeight/useMaxHeight.ts
@@ -1,10 +1,13 @@
 import { onMounted, onUnmounted, ref } from 'vue'
+function getHeight(el: HTMLElement, footerHeight: number = 0) {
+  const rect = el.getBoundingClientRect()
+  return window.innerHeight - rect.y - footerHeight
+}
 export function useMaxHeight(el: HTMLElement | null, footerHeight: number = 0) {
   const height = ref(0)
   function setHeight() {
     if (el) {
-      const rect = el.getBoundingClientRect()
-      height.value = window.innerHeight - rect.y - footerHeight
+      height.value = getHeight(el, footerHeight)
     }
   }
   onMounted(async () => {
