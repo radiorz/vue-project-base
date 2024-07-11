4c140fc13949d2ed159ff49d19217a2272b1d43a
tikkhun <s565338107@163.com>
2024-07-11 17:42:55 +0800

    chore: 可拖拽 可缩放。

---
+++ packages/vue-ui/lib/DragResize/DragResize.vue
@@ -0,0 +1,41 @@
+<!--
+ 
+/**
+ * @author
+ * @file DragResize.vue
+ * @fileBase DragResize
+ * @path packages\vue-ui\lib\DragResize\DragResize.vue
+ * @from 
+ * @desc 
+ * @todo
+ * 
+ *
+ * @done
+ * @example
+ */
+
+-->
+
+<script setup lang="ts">
+defineOptions({
+  name: 'DragResize'
+})
+export interface Props {
+  foo: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  foo: ''
+})
+export interface Emits {
+  (e: 'change', value: number): void
+}
+const emit = defineEmits<Emits>()
+</script>
+
+<template>
+  <div class="DragResize">
+    <slot :width="width" :height="height"></slot>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ packages/vue-ui/lib/Draggable/Draggable.vue
@@ -0,0 +1,41 @@
+<!--
+ 
+/**
+ * @author
+ * @file Draggable.vue
+ * @fileBase Draggable
+ * @path packages\vue-ui\lib\Draggable\Draggable.vue
+ * @from 
+ * @desc 
+ * @todo
+ * 
+ *
+ * @done
+ * @example
+ */
+
+-->
+
+<script setup lang="ts">
+import { ref } from 'vue'
+defineOptions({
+  name: 'TheDraggable'
+})
+export interface Props {
+  foo: string
+}
+const props = withDefaults(defineProps<Props>(), {
+  foo: ''
+})
+export interface Emits {
+  (e: 'change', value: number): void
+}
+const emit = defineEmits<Emits>()
+const el = ref(null)
+</script>
+
+<template>
+  <div ref="el">Draggable</div>
+</template>
+
+<style lang="scss" scoped></style>
