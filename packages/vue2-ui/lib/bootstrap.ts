ae075c87abe4a2932df514ac8a7a4657a9dc81e2
tikkhun <s565338107@163.com>
2024-10-29 16:46:00 +0800

    chore: update

--- packages/vue2-ui/lib/bootstrap.ts
+++ packages/vue2-ui/lib/bootstrap.ts
@@ -1,6 +1,3 @@
-// import 'element-ui/lib/theme-chalk/index.css';
+import 'element-ui/lib/theme-chalk/index.css';
 // import ElementUI from 'element-ui';
-import Vue from 'vue'
-import VueCompositionAPI from '@vue/composition-api'
-Vue.use(VueCompositionAPI)
 // Vue.use(ElementUI);
